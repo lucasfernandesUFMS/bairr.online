47582c62963cc2508da89901f92b3982cb42660d
lucasfernandesUFMS <lucas.fernandes@ufms.br>
2025-07-13 00:51:18 -0400

    conexao.php V2.0
    Adicionados os recursos de pesquisa por serviços e paginação de serviços.

--- conexao.php
+++ conexao.php
@@ -1,11 +1,16 @@
 <?php
-$host = "10.132.36.3";
-$usuario = "bairr1c81f229_lucas";
-$senha = "projetointegrador";
-$banco = "bairr1c81f229_bairro_online";
+// Configurações de acesso ao banco de dados no servidor UOL Host
+$host     = "10.132.36.3";
+$usuario  = "bairr1c81f229_lucas";
+$senha    = "projetointegrador";
+$banco    = "bairr1c81f229_bairro_online";
 
+// Cria conexão com o MySQL
 $conn = mysqli_connect($host, $usuario, $senha, $banco);
+
+// Verifica se ocorreu erro de conexão
 if (!$conn) {
-  die("Erro na conexao: " . mysqli_connect_error());
+  // Exibe mensagem personalizada e segura
+  die("Falha ao conectar com o banco de dados. Tente novamente mais tarde.");
 }
 ?>
